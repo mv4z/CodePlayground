ffd1b4d39ba329e14af8478a6ff981ab282d4f78
mazzquezzz <50344513+mazzquezzz@users.noreply.github.com>
2020-04-28 22:44:47 -0500

    Added several different directories

--- MOOC/075.DecreasingCounter-4631228/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(10);
-
-        counter.printValue();
-        counter.decrease();
-        counter.decrease();
-    }
-}
--- MOOC/083.HangmanLogic-4663669/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        HangmanLogic logic = new HangmanLogic("parameter");
-        HangmanUserInterface game = new HangmanUserInterface(logic);
-        game.start();
-    }
-}
